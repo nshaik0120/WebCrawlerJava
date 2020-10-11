ca914f889af77dd48942570073acf0cad4cb45d0
nshaik0120 <45870652+nshaik0120@users.noreply.github.com>
2020-10-11 23:33:57 +0800

    Update WebcrawlerApplication.java
    checking string from url

--- Webcrawler/src/main/java/com/example/Webcrawler/WebcrawlerApplication.java
+++ Webcrawler/src/main/java/com/example/Webcrawler/WebcrawlerApplication.java
@@ -1,6 +1,10 @@
 package com.example.Webcrawler;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.HashSet;
 
 import org.jsoup.Jsoup;
@@ -23,7 +27,7 @@ public class WebcrawlerApplication {
 	    }
 
 	
-	 public void getPageLinks(String URL, int depth) {
+	 public HashSet<String> getPageLinks(String URL, int depth) {
 		 //Limiting the size of  10 URLS from yahoo site
 	        if (!links.contains(URL) && links.size()<10 ) {
 	            System.out.println("Fecthing the Urls from Yahoo Site"+ "  "+depth+" :"+ "[" + URL + "]");
@@ -41,11 +45,44 @@ public class WebcrawlerApplication {
 	                System.err.println("For '" + URL + "': " + e.getMessage());
 	            }
 	        }
+			return links;
 	    }
+	 
+	 public static String getText(String url) throws Exception {
+		    URL website = new URL(url);
+		    URLConnection connection = website.openConnection();
+		    BufferedReader in = new BufferedReader(
+		                                new InputStreamReader(
+		                                    connection.getInputStream()));
+
+		    StringBuilder response = new StringBuilder();
+		    String inputLine;
+
+		    while ((inputLine = in.readLine()) != null) 
+		        response.append(inputLine);
+
+		    in.close();
+
+		    return response.toString();
+		}
 
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebcrawlerApplication.class, args);
+		
+		String content = null;
+		try {
+			content = new WebcrawlerApplication().getText("https://sg.yahoo.com/?p=us&guccounter=1");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		if(content.contains("smartphone"))
+		{
+		    // content of url contains keyword
+			
+			System.out.println("content="+content);
+		}
 		
 	   
 	        new WebcrawlerApplication().getPageLinks("http://yahoo.com/", 0);
